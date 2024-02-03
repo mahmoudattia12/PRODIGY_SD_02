06d12a5823004c75256bd599ef476736ea02ab11
mahmoudattia12 <es-mahmoudattia2025@alexu.edu.eg>
2024-02-03 21:05:18 +0200

    add game icon

--- Guessing Game/src/main/java/com/example/guessinggame/GuessingGameApp.java
+++ Guessing Game/src/main/java/com/example/guessinggame/GuessingGameApp.java
@@ -3,6 +3,7 @@ package com.example.guessinggame;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import java.io.IOException;
 
@@ -12,6 +13,8 @@ public class GuessingGameApp extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(GuessingGameApp.class.getResource("GuessingGame-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1100, 500);
         stage.setTitle("Guessing Game!");
+        Image icon = new Image(String.valueOf(getClass().getResource("dice_PNG54.png")));
+        stage.getIcons().add(icon);
         stage.setScene(scene);
         stage.show();
     }
