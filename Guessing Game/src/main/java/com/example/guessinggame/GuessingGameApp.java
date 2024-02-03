3c180a95e453f9ef4614b378acedd5abc93e011d
mahmoudattia12 <es-mahmoudattia2025@alexu.edu.eg>
2024-02-03 21:06:46 +0200

    edit game title

--- Guessing Game/src/main/java/com/example/guessinggame/GuessingGameApp.java
+++ Guessing Game/src/main/java/com/example/guessinggame/GuessingGameApp.java
@@ -12,7 +12,7 @@ public class GuessingGameApp extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(GuessingGameApp.class.getResource("GuessingGame-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1100, 500);
-        stage.setTitle("Guessing Game!");
+        stage.setTitle("Guessing Game");
         Image icon = new Image(String.valueOf(getClass().getResource("dice_PNG54.png")));
         stage.getIcons().add(icon);
         stage.setScene(scene);
